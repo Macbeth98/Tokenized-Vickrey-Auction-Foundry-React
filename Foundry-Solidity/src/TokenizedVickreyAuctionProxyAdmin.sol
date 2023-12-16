00adfa947719b567e56b2e53588c055c9baba157
Macbeth98 <manichandra.teja@gmail.com>
2023-12-16 02:29:14 -0500

    chore: refactoring removed modules

--- Foundry-Solidity/src/TokenizedVickreyAuctionProxyAdmin.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.20;
-
-import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-
-contract TokenizedVickeryAuctionProxyAdmin is ProxyAdmin {
-    constructor(address initialOwner) ProxyAdmin(initialOwner) {}
-}
